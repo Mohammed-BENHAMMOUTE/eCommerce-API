4c67c5506c8776f30a4f609872359405f2eb6693
Mohammed-BENHAMMOUTE <mohammedbenhammoute@gmail.com>
2025-02-02 12:40:25 +0100

    first commit

---
+++ src/main/java/org/sec/ecommerce/model/Category.java
@@ -0,0 +1,12 @@
+package org.sec.ecommerce.model;
+
+import java.util.List;
+
+public class Category {
+    private Long id;
+    private String name;
+
+
+    private List<Product> products;
+
+}
---
+++ src/main/java/org/sec/ecommerce/model/Image.java
@@ -0,0 +1,5 @@
+package org.sec.ecommerce.model;
+
+public class Image {
+
+}
