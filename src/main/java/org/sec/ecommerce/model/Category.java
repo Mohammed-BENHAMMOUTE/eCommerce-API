e9d97901602f502675dee80f268a3f8821f34856
Mohammed-BENHAMMOUTE <mohammedbenhammoute@gmail.com>
2025-02-02 13:01:04 +0100

    adding Products relationships

--- src/main/java/org/sec/ecommerce/model/Category.java
+++ src/main/java/org/sec/ecommerce/model/Category.java
@@ -1,12 +1,22 @@
 package org.sec.ecommerce.model;
 
-import java.util.List;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.util.List;
+@Entity
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-
-
+    @OneToMany(mappedBy = "category")
     private List<Product> products;
-
 }
--- src/main/java/org/sec/ecommerce/model/Image.java
+++ src/main/java/org/sec/ecommerce/model/Image.java
@@ -1,5 +1,26 @@
 package org.sec.ecommerce.model;
 
-public class Image {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.sql.Blob;
+
+@Getter @Setter @AllArgsConstructor
+@Entity @NoArgsConstructor
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fileName;
+    private String fileType;
+    @Lob
+    private Blob image;
+    private String downloadUrl;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
 }
